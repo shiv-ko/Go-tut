fba79d95970c404a1c2d5343a4aeecf40ea4c67b
shiv-ko <iscopre7@gmail.com>
2024-07-12 11:16:51 +0900

    added path parameter

--- gotoapi/handlers/handlers.go
+++ gotoapi/handlers/handlers.go
@@ -17,7 +17,11 @@ func ArticleListHandler(w http.ResponseWriter,req *http.Request){
   io.WriteString(w,"Article List\n")
 }
 func ArticleDetailHandler(w http.ResponseWriter,req *http.Request){
-  articleID:=1
+  articleID,err:=strconv.Atoi(mux.Vars(req)["id"])
+  if err!=nil{
+    http.Error(w,"Invalid query parameter",http.StatusBadRequest)
+    return
+  }
   resString:=fmt.Sprintf("Article No.%d\n",articleID)
   io.WriteString(w,resString)
 }
