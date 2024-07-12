8dcd730ebe5633b19665553955ac63b43b5c0ca9
shiv-ko <iscopre7@gmail.com>
2024-07-12 09:51:08 +0900

    fixed handler-name

--- gotoapi/main.go
+++ gotoapi/main.go
@@ -7,12 +7,11 @@ import (
 ) 
   func main(){
     http.HandleFunc("/hello",handlers.helloHandler)
-    http.HandleFunc("/article",handlers.postArticleHandler)
-    http.HandleFunc("/article/list",handlers.articleListHandler)
-    http.HandleFunc("/article/1",handlers.bestArticleHandler)
-    http.HandleFunc("/article/nice",handlers.niceArticleHandler)
-    http.HandleFunc("/comment",handlers.commentArticleHandler)
+    http.HandleFunc("/article",handlers.PostArticleHandler)
+    http.HandleFunc("/article/list",handlers.ArticleListHandler)
+    http.HandleFunc("/article/1",handlers.BestArticleHandler)
+    http.HandleFunc("/article/nice",handlers.NiceArticleHandler)
+    http.HandleFunc("/comment",handlers.CommentArticleHandler)
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080",nil))
-        
 }
