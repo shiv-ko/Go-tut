84aa0b69a45f3f347a7d2dc8d778dcc1b4086812
shiv-ko <iscopre7@gmail.com>
2024-07-14 07:01:10 +0900

    fixed handler

--- gotoapi/handlers/handlers.go
+++ gotoapi/handlers/handlers.go
@@ -15,23 +15,24 @@ import (
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello World\n")
 }
+
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	article := models.Article1
-	jsondData, err := json.Marshal(article)
+	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "faild to encode json\n", http.StatusInternalServerError)
 		return
 	}
 	w.Write(jsonData)
-
 }
+
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
 	var page int
 
 	if p, ok := queryMap["page"]; ok && len(p) > 0 {
 		var err error
-		//to int
+		// to int
 		page, err = strconv.Atoi(p[0])
 		// not int
 		if err != nil {
@@ -40,15 +41,16 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		}
 
 	} else {
-		//クエリパラケータがないときに１に設定する
+		// クエリパラケータがないときに１に設定する
 		page = 1
 	}
 	resString := fmt.Sprintf("Article List(page %d)\n", page)
 	io.WriteString(w, resString)
 }
+
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	//varsはパスパラメタをMap形式で返す
-	//Atoi=string→int
+	// varsはパスパラメタをMap形式で返す
+	// Atoi=string→int
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	///数字じゃない場合のエラー
 	if err != nil {
@@ -58,9 +60,11 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
+
 func NiceArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Nice...\n")
 }
+
 func CommentArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Comment...\n")
 }
