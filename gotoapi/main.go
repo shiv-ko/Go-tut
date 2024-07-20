6d492fd3d0deefcfbea4f4f100d9dde4a7094096
shiv-ko <iscopre7@gmail.com>
2024-07-21 08:33:41 +0900

    fixed handler

--- gotoapi/main.go
+++ gotoapi/main.go
@@ -1,19 +1,20 @@
-package main 
+package main
 
-import ( 
-  "log"
-  "net/http" 
-  "github.com/shiv-ko/Go-tut/gotoapi/handlers"
-  "github.com/gorilla/mux"
-) 
-  func main(){
-    r:=mux.NewRouter()
-    r.HandleFunc("/hello",handlers.HelloHandler).Methods(http.MethodGet)
-    r.HandleFunc("/article",handlers.PostArticleHandler).Methods(http.MethodPost)
-    r.HandleFunc("/article/list",handlers.ArticleListHandler).Methods(http.MethodGet)
-    r.HandleFunc("/article/{id:[0-9]+}",handlers.ArticleDetailHandler).Methods(http.MethodGet)
-    r.HandleFunc("/article/nice",handlers.NiceArticleHandler).Methods(http.MethodPost)
-    r.HandleFunc("/comment",handlers.CommentArticleHandler).Methods(http.MethodPost)
-    log.Println("server start at port 8080")
-    log.Fatal(http.ListenAndServe(":8080",r))
+import (
+	"github.com/gorilla/mux"
+	"github.com/shiv-ko/Go-tut/gotoapi/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.NiceArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.CommentArticleHandler).Methods(http.MethodPost)
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- hello.go
+++ hello.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Printf("Hello World!\n")
-  
+	fmt.Printf("Hello World!\n")
 }
