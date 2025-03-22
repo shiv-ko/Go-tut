dc5f6dea8c26259397f651b6831841c7428a8296
shiv-ko <iscopre7@gmail.com>
2025-03-22 17:50:13 +0900

    ディレクトリを浅くする

--- DB/go.mod
+++ DB/go.mod
@@ -5,5 +5,5 @@ go 1.22.5
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/shiv-ko/Go-tut/gotoapi v0.0.0-20240728202812-4e4eb260eb88 // indirect
+	github.com/shiv-ko/Go-tut/gotoapi/DB v0.0.0-20240728202812-4e4eb260eb88 // indirect
 )
---
+++ DB/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/shiv-ko/Go-tut/gotoapi/DB/models"
+)
+
+func main() {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
+		dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//現在のいいね数を取得するクエリ
+	article_id := 1
+	const sqlGetNice = `
+	select nice from articles where article_id = ?
+	`
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//変数nicenumに現在のいいね数を格納
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err := row.Scan(&nicenum); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//いいね数を1増やすクエリ
+	const sqlUpdateNice = `
+	update articles set nice = ? where article_id = ?
+	`
+
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		//エラーならロールバックして処理を取り消す
+		tx.Rollback()
+		return
+	}
+	//コミットして処理内容を確定
+	tx.Commit()
+}
