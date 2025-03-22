43ecaa48913e522df2309844b2293b0736960db1
shiv-ko <iscopre7@gmail.com>
2025-03-22 16:36:54 +0900

    データを挿入する処理

--- gotoapi/DB/main.go
+++ gotoapi/DB/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/shiv-ko/Go-tut/gotoapi/models"
 )
 
 func main() {
@@ -19,12 +19,15 @@ func main() {
 		fmt.Println(err)
 	}
 	defer db.Close()
+	articleID := 1
+	// SQL文を定義
 	const sqlStr = `
-		select title ,contents,username,nice
-		from articles;
+		select *
+		from articles
+		where article_id = ?;
 		`
-	// 実行したいSQLぶんを引数に指定すると実行結果としてもらえる
-	rows, err := db.Query(sqlStr)
+	// 実行したいSQL文を引数に指定すると実行結果としてもらえる
+	rows, err := db.Query(sqlStr, articleID)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -32,12 +35,19 @@ func main() {
 	defer rows.Close()
 
 	// rowsからデータを読み出して構造体に格納する
-	articleArray := make([]models.Ariticle, 0)
+	articleArray := make([]models.Article, 0)
 	for rows.Next() {
+		//　記事データ（１行分）を格納する構造体を定義
 		var article models.Article
-		// articleのフィールドないに取得したデータを入れる
-		err := rows.Scan(&article.Title, &article.Contents, &article.UserName,
-			&article.NiceNum)
+		// Nullかもしれないデータを格納するための変数を定義
+		var createdTime sql.NullTime
+		// articleのフィールド内に取得したデータを入れる
+		err := rows.Scan(&article.ID, &article.Title, &article.Content, &article.UserName,
+			&article.NiceNum, &createdTime)
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
 		if err != nil {
 			fmt.Println(err)
 		} else {
